5ecbaeaf458729f085c0e6f87b80f291198016b2
avcopan <avcopan@gmail.com>
2018-04-07 18:09:53 -0400

    Adds script for plotting memory profiles. Also learning to use TidyVerse more consistently, and trying to specify namespaces explicitly for better understanding.

---
+++ line.R
@@ -0,0 +1,20 @@
+library(ggplot2)
+library(readr)
+library(dplyr)
+library(ggthemes)
+
+d <- readr::read_delim('memory.txt', delim=' ',
+                       col_names=c('m', 't'),
+                       col_types=c(col_double(), col_double()))
+d <- dplyr::mutate(d, dt=t-t[[1]])
+print(typeof(d))
+print(attributes(d))
+print(d)
+
+p <- (ggplot2::ggplot(d) +
+      ggplot2::geom_line(mapping=ggplot2::aes_(x=quote(dt), y=quote(m)), colour="red") +
+      ggplot2::xlab('time (s)') +
+      ggplot2::ylab('memory (mb)') +
+      ggthemes::theme_base())
+
+ggplot2::ggsave(file='mem.pdf', plot=p, width=10, height=5)
